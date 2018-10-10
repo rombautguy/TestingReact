1e7548a93b122fd89be550fe43614f3a130543be
Guy <rombautguy@gmail.com>
2018-10-10 22:11:12 +0200

    clean

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './Components/MainTemplate/App';
-import {unregister} from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
 
-unregister();
 ReactDOM.render(
   <BrowserRouter>
     <App />
